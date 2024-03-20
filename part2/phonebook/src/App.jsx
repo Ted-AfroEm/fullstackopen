084b3dd487c3152418317277a341cb20ea157494
Tedros Kahsu <tedkahcom@gmail.com>
2024-03-20 09:50:38 +0300

    Solve part1 and part2

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const addName = (event) => {
+    event.preventDefault();
+    const nameExist = persons.find((person) => person.name === newName);
+    if (nameExist === undefined) {
+      setPersons([...persons, { name: newName }]);
+      setNewName("");
+    } else {
+      alert(`${newName} is already added to phonebook`);
+    }
+  };
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <div>debug: {newName}</div>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
