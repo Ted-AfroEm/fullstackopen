f41b3d95af6a605c1d7345aa7e4a2320e36866e0
Tedros Kahsu <tedkahcom@gmail.com>
2024-03-22 08:41:16 +0300

    Server and axios

--- part2/part2/src/App.jsx
+++ part2/part2/src/App.jsx
@@ -1,14 +1,47 @@
-const App = (props) => {
-  const { notes } = props;
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Note from "./components/Note";
+
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(true);
+  useEffect(() => {
+    axios.get("http://localhost:3001/notes").then((response) => {
+      setNotes(response.data);
+    });
+  }, []);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
+    setNotes([...notes, noteObject]);
+    setNewNote("");
+  };
+  const handleChange = (event) => {
+    setNewNote(event.target.value);
+  };
+
+  const notesToshow = showAll
+    ? notes
+    : notes.filter((note) => note.important === true);
 
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        <li>{notes[0].content}</li>
-        <li>{notes[1].content}</li>
-        <li>{notes[2].content}</li>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   );
 };
